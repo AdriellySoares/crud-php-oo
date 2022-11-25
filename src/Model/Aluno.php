97681d67c8da51eb13fa1b28ded5e58bb428215a
adriellysoares <AdriellySoares>
2022-11-25 10:17:26 -0300

    adicionando arquivos

---
+++ src/Model/Aluno.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model;
+
+use DateTime; //importando a classe interna do PHP DateTime
+
+//aqui vai ficar a definição do caminho até essa classe
+
+class Aluno extends Pessoa
+{
+    public string $nome;
+    public string $cpf;
+    public int $matricula;
+    public string $dataNascimento;
+    public bool $status;
+    public string $genero; //enum
+
+    // public function __construct(string $nome, string $dataNascimento)
+    // {
+    //     $this -> $nome = ucwords(strtolower($nome));
+    //     $this -> $dataNascimento = $dataNascimento;
+    // }
+}
